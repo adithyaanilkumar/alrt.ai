7cdf30ccd67e15b25bd91eb5f9ce267fbfc26470
adithyaanilkumar <adithyaanilkumar1@gmail.com>
2019-12-31 13:40:03 +0530

    added  more hl and gl tags

--- scrape.py
+++ scrape.py
@@ -4,19 +4,19 @@ import re
 
 url="https://www.autoitscript.com/autoit3/docs/appendix/OSLangCodes.htm"
 html= urllib.request.urlopen(url).read()
-soup=BeautifulSoup(html,'html.parser')
-
-tags= soup.find_all('td')
+soup=BeautifulSoup(html,'lxml')
+print(soup.get_text())
+tags= soup.find('td').find_all(text=True)
 lst= list()
 c=0
-
-fhand = open("data.xml", "w")
+print([e.strip() for e in tags])
+'''fhand = open("data.xml", "w")
 for tag in tags:
     print(tag)
     fhand.write(str(tag))
     c+=1
     if c>= 50:
-        break
+        break'''
 '''del lst[0]
 del lst[1]'''
 '''for i in range(length(lst)):
